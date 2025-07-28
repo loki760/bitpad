9647fb7c098b4a1562a7e0c7a6c37c5fdc033c07
loki760 <mr.shlokupadhyay@gmail.com>
2025-07-28 21:33:42 +0530

    enter mapped to create new line

--- kilo.c
+++ kilo.c
@@ -291,11 +291,14 @@ void editorUpdateRow(erow *row) // copy chars in render string
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
-    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1)); // multiply the number of bytes each erow takes and multiply that by the number of rows
+    if (at < 0 || at > E.numrows)
+        return;
 
-    int at = E.numrows; // set the index to line no.
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -317,6 +320,87 @@ void editorFreeRow(erow *row)
 
 void editorDelRow(int at)
 {
+    if (at < 0 || at >= E.numrows)
+        return;
+
+    editorFreeRow(&E.row[at]);
+    memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+    E.numrows--;
+    E.dirty++;
+}
+
+void editorRowInsertChar(erow *row, int at, int c) // at is index at which char is inserted
+{
+    if (at < 0 || at > row->size)
+        at = row->size;
+
+    row->chars = realloc(row->chars, row->size + 2); // +2 for null byte too
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+
+    row->size++;
+    row->chars[at] = c;
+
+    editorUpdateRow(row); // to update render and rsize
+}
+
+void editorRowDelChar(erow *row, int at)
+{
+    if (at < 0 || at >= row->size)
+        return;
+
+    memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+    row->size--;
+
+    editorUpdateRow(row);
+
+    E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len)
+{
+    row->chars = realloc(row->chars, row->size + len + 1);
+    memcpy(&row->chars[row->size], s, len);
+    row->size += len;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+    E.dirty++;
+}
+
+/***    editor operations   ***/
+
+void editorInsertChar(int c)
+{
+    if (E.cy == E.numrows) // cursor is on ~ line after EOF, need to append new row before inserting character
+    {
+        editorInsertRow(E.numrows, "", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
+void editorInsertNewline()
+{
+    if (E.cx == 0)
+    {
+        editorInsertRow(E.cy, "", 0);
+    }
+
+    else
+    {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+
+    E.cy++;
+    E.cx = 0;
+}
+
+void editorDelChar()
+{
     if (E.cy == E.numrows)
         return;
 
@@ -337,69 +421,6 @@ void editorDelRow(int at)
         editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
         editorDelRow(E.cy);
         E.cy--;
-    }
-}
-
-void editorRowInsertChar(erow *row, int at, int c) // at is index at which char is inserted
-{
-    if (at < 0 || at > row->size)
-        at = row->size;
-
-    row->chars = realloc(row->chars, row->size + 2); // +2 for null byte too
-    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
-
-    row->size++;
-    row->chars[at] = c;
-
-    editorUpdateRow(row); // to update render and rsize
-}
-
-void editorRowAppendString(erow *row, char *s, size_t len)
-{
-    row->chars = realloc(row->chars, row->size + len + 1);
-    memcpy(&row->chars[row->size], s, len);
-    row->size += len;
-    row->chars[row->size] = '\0';
-    editorUpdateRow(row);
-    E.dirty++;
-}
-
-void editorRowDelChar(erow *row, int at)
-{
-    if (at < 0 || at >= row->size)
-        return;
-
-    memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
-    row->size--;
-
-    editorUpdateRow(row);
-
-    E.dirty++;
-}
-
-/***    editor operations   ***/
-
-void editorInsertChar(int c)
-{
-    if (E.cy == E.numrows) // cursor is on ~ line after EOF, need to append new row before inserting character
-    {
-        editorAppendRow("", 0);
-    }
-    editorRowInsertChar(&E.row[E.cy], E.cx, c);
-    E.cx++;
-}
-
-void editorDelChar()
-{
-    if (E.cy == E.numrows)
-        return;
-
-    erow *row = &E.row[E.cy];
-
-    if (E.cx > 0)
-    {
-        editorRowDelChar(row, E.cx - 1);
-        E.cx--;
     }
 }
 
@@ -445,7 +466,7 @@ void editorOpen(char *filename)
     {
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) // we're stripping carriage and newline cuz erow reps one line of text
             linelen--;
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
 
     free(line);
@@ -709,7 +730,7 @@ void editorProcessKeypress()
     {
 
     case '\r':
-        // TO DO
+        editorInsertNewline();
         break;
 
     case CTRL_KEY('q'):
